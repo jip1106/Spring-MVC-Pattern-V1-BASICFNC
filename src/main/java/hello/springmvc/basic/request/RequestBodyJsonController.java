d6804a1ebcd70b12f0108697734210fe585224a1
jipark1106@gmail.com <jipark1106@gmail.com>
2023-11-28 20:48:37 +0900

    home

---
+++ src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -0,0 +1,83 @@
+package hello.springmvc.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.springmvc.basic.HelloData;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * {"usernae" : "hello , "age":20}
+ * content-type: application/json
+ */
+@Slf4j
+@Controller
+public class RequestBodyJsonController {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    @PostMapping("/request-body-json-v1")
+    public void requestBodyJsonV1(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}",messageBody);
+
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        log.info("username={}, age={} " , helloData.getUsername(), helloData.getAge());
+
+        response.getWriter().write("ok");
+    }
+
+
+    @PostMapping("/request-body-json-v2")
+    @ResponseBody
+    public String requestBodyJsonV2(@RequestBody String messageBody) throws Exception{
+
+        log.info("messageBody={}",messageBody);
+
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        log.info("username={}, age={} " , helloData.getUsername(), helloData.getAge());
+
+        return "ok";
+    }
+
+    @PostMapping("/request-body-json-v3")
+    @ResponseBody
+    public String requestBodyJsonV3(@RequestBody HelloData data) throws Exception{
+
+        log.info("username={}, age={} " , data.getUsername(), data.getAge());
+
+        return "ok";
+    }
+
+    @PostMapping("/request-body-json-v4")
+    @ResponseBody
+    public String requestBodyJsonV4(HttpEntity<HelloData> httpEntity) throws Exception{
+        HelloData data = httpEntity.getBody();
+        log.info("username={}, age={} " , data.getUsername(), data.getAge());
+
+        return "ok";
+    }
+
+    @PostMapping("/request-body-json-v5")
+    @ResponseBody
+    public HelloData requestBodyJsonV5(@RequestBody HelloData data) throws Exception{
+
+        log.info("username={}, age={} " , data.getUsername(), data.getAge());
+
+        return data;
+    }
+
+
+
+
+}
---
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,134 @@
+package hello.springmvc.basic.request;
+
+import hello.springmvc.basic.HelloData;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        log.info("username ={}, age={}" , username ,age);
+
+        response.getWriter().write("ok");
+    }
+
+    @RequestMapping("/request-param-v2")
+    @ResponseBody
+    public String requestParamV2(@RequestParam("username") String memberName,
+                               @RequestParam("age") int memberAge) throws IOException {
+
+        log.info("username ={}, age={}" , memberName ,memberAge);
+
+        return "ok";
+    }
+
+    //앞에서 넘어오는 name이랑(요청파라미터) 받는 파라미터명이 같으면 @RequestParam만 사용 가능
+    @RequestMapping("/request-param-v3")
+    @ResponseBody
+    public String requestParamV3(@RequestParam String username,
+                                 @RequestParam int age) throws IOException {
+
+        log.info("username ={}, age={}" , username ,age);
+
+        return "ok";
+    }
+    
+    //앞에서 넘어오는 name이랑(요청파라미터) 받는 파라미터명이 같고, String, int, Integer 등의 단순 타입이면
+    // 변수명으로만 받을 수도 있음
+    @RequestMapping("/request-param-v4")
+    @ResponseBody
+    public String requestParamV4(String username, int age) throws IOException {
+
+        log.info("username ={}, age={}" , username ,age);
+
+        return "ok";
+    }
+
+
+    //@RequestParam 의 기본 required는 true값
+    //@RequestParam 이 false이면 요청 파라미터에 없어도 되지만 기본값이 null로 들어온다.
+    @RequestMapping("/request-param-required")
+    @ResponseBody
+    public String requestParamRequired(@RequestParam(required = true) String username,
+                                       @RequestParam(required = false) Integer age) throws IOException {
+
+        log.info("username ={}, age={}" , username ,age);
+
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-default")
+    @ResponseBody
+    public String requestParamDefault(@RequestParam(required = true, defaultValue = "guest") String username,
+                                       @RequestParam(required = false, defaultValue = "-1") int age) {
+
+        log.info("username ={}, age={}" , username ,age);
+
+        return "ok";
+    }
+
+    @RequestMapping("/request-param-list")
+    @ResponseBody
+    public String requestParamMap(@RequestParam List<String> paramList) {
+        for (String tmp : paramList){
+            log.info("tmp : {} " ,paramList);
+        }
+
+//        log.info("username ={}, age={}" , paramMap.get("username") ,paramMap.get("age"));
+
+        return "ok";
+    }
+    @RequestMapping("/request-param-map")
+    @ResponseBody
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
+        for (String key : paramMap.keySet()) {
+            log.info("key={}, value={}", key, paramMap.get(key));
+        }
+
+//        log.info("username ={}, age={}" , paramMap.get("username") ,paramMap.get("age"));
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(/*@RequestParam String username , @RequestParam int age*/
+                                @ModelAttribute HelloData helloData){
+
+        log.info("HelloData = {}", helloData);
+        return "ok";
+    }
+
+    /**
+     * @ModelAttribute 는 생략 가능하다.
+     * 스프링은 String, int Integer 등 단순 타입은  @RequestParam 으로 인식하고
+     * 나머지는 ModelAttribute로 인식한다 (argument resolver 제외)
+     * @param helloData
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData){
+
+        log.info("HelloData = {}", helloData);
+        return "ok";
+    }
+
+
+}
